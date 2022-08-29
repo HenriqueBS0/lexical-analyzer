fa875bdf4a2b37c57215f265203059e8de9f4bd8
Henrique Borges dos Santos <henrique.10agr@gamil.com>
2022-08-29 07:45:58 -0300

    Created lexical analyzer class

---
+++ src/LexicalAnalyzer.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace HenriqueBS0\LexicalAnalyzer;
+
+use HenriqueBS0\Automaton\Automaton;
+use HenriqueBS0\Automaton\AutomatonException;
+
+class LexicalAnalyzer {
+    private Automaton $automaton;
+
+    public function __construct(/*Automaton $automaton */)
+    {
+        // $this->automaton = $automaton;
+    }
+
+    private function getAutomaton() : Automaton 
+    {
+        return $this->automaton;
+    }
+
+    public function getTokens(string $input) : TokenStack 
+    {
+        $tokens = new TokenStack();
+
+        $characteres = str_split($input);
+
+        $startReadingPosition = 1;
+        $readPosition         = 1;
+        $endReadingPosition   = count($characteres);
+
+        $token = null;
+
+        while($readPosition <= $endReadingPosition) {
+            $partInput = self::getPartInput($characteres, $startReadingPosition, $readPosition);
+
+            $position = Position::get($characteres, $startReadingPosition, $readPosition);
+
+            try {
+                $finalState = $this->getAutomaton()->getFinalState($partInput); 
+                $token = new Token($finalState, $partInput, $position);
+            }
+            catch(AutomatonException $ex) {
+
+                $unexpectedCharacter  = $ex->getCode() === AutomatonException::CODE_UNEXPECTED_INPUT_CHARACTER;
+                $noValidToken         = is_null($token);
+
+                if($unexpectedCharacter || $noValidToken) {
+                    throw new LexicalAnalyzerException($position, $tokens, $ex->getMessage());
+                }
+
+                $tokens->push($token);
+                $startReadingPosition = $token->getPosition()->getEndPosition() + 1;
+                $readPosition = $token->getPosition()->getEndPosition() + 1;
+            }
+
+            if($readPosition === $endReadingPosition) {
+                $tokens->push($token);
+            }
+        }
+
+        return $tokens;
+    }
+
+    private static function getPartInput(array $characteres, int $startPostion, int $endPosition) {
+        return implode(array_splice($characteres, ($startPostion - 1), (($endPosition + 1) - $startPostion)));
+    }
+}
